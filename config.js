61a741daa969cf392c03b01a91157976d8b46b5c
aliasccl <george@crosschainlabs.tech>
2021-08-27 02:26:13 +0300

    implemented retry

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
     port: process.env.NEAR_LINK_PORT || 80,
+    maxAttempts: process.env.MAX_ATTEMPTS || 5,
     nearlinknameserver: process.env.NEAR_LINK_NAME_SERVER || ''
 };
--- index.js
+++ index.js
@@ -1,50 +1,66 @@
 const config = require('./config');
 const express = require('express')
+const util = require('util');
+const Resolver = require('dns');
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
-
-
-const { Resolver } = require('dns');
-const resolver = new Resolver();
-var cors = require('cors');
-
-resolver.setServers([config.nearlinknameserver]);
-
 const app = express();
 const port = config.port;
+const maxAttempts = config.maxAttempts;
+
+var cors = require('cors');
+
+Resolver.setServers([config.nearlinknameserver]);
+const resolveTxtAsync = util.promisify(Resolver.resolveTxt);
 
 app.use(cors())
 
-app.get('*', (req, res) => {
-    console.log(req.subdomains);  //test
-    console.log(req.hostname);    //test.example.com
+app.get('/', async (req, res) => {
+  console.log(req.subdomains);  //test
+  console.log(req.hostname);    //test.example.com
 
-    // This request will use the server at 4.4.4.4, independent of global settings.
-resolver.resolveTxt(req.hostname, (err, addresses) => {
-    console.log(addresses, err);
+  const execute = async (attempt) => {
+    try {
+      return await resolveTxtAsync(req.hostname);
+    } catch (err) {
+      if (attempt <= maxAttempts) {
+        const nextAttempt = attempt + 1;
+        console.error(`Retrying due to:`, err)
+        return execute(nextAttempt);
+      } else {
+        console.error(`Error:`, err)
+      }
+    }
+  }
 
-    JSDOM.fromURL("https://github.com/jsdom/jsdom", {
-      //url: "https://example.org/",
-      //referrer: "https://example.com/",
-      //contentType: "text/html",
-      includeNodeLocations: true,
-      storageQuota: 10000000
-    }).then(dom => {
-      //console.log(dom.serialize());
-      res.status(200).send(dom.serialize());
-    });
+  try {
+    const addresses = await execute(1);
+    console.log("addresses", addresses);
 
+    if (addresses?.length > 0) {
+      if (addresses[0]?.length > 0) {
+        console.log(addresses[0][0]);
+        JSDOM.fromURL("https://github.com/jsdom/jsdom", {
+          //url: "https://example.org/",
+          //referrer: "https://example.com/",
+          //contentType: "text/html",
+          includeNodeLocations: true,
+          storageQuota: 10000000
+        }).then(dom => {
+          //console.log(dom.serialize());
+          res.status(200).send(dom.serialize());
+        });
+      }
+    }
 
-    //res.status(200).send('<script>window.location.href="https://ipfs.io/ipfs/f01701220c3c4733ec8affd06cf9e9ff50ffc6bcd2ec85a6170004bb709669c31de94391a"</script>');
-    
-    // ...
-  });
-  
-  //res.send('Hello World!')
+  } catch (error) {
+    console.log(error);
+  }
+
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://test.near.link:${port}`)
 })
 
 
